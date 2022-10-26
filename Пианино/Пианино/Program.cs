fd4e07e06585513682cdd0268af3afa1a1380a84
Railwin <Railwin10000@gmail.com>
2022-10-26 10:29:16 +0300

    программы вторая практическая

---
+++ Пианино/Пианино/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Drawing;
+Console.WriteLine("Переключение по октавам с помощью f3,f4,f5,f6");
+while (true)
+{
+    ConsoleKeyInfo clav = Console.ReadKey();
+    if (clav.Key == ConsoleKey.F3)
+    {
+        while (clav.Key != ConsoleKey.F4 && clav.Key != ConsoleKey.F5 && clav.Key != ConsoleKey.F6)
+        {
+            Console.WriteLine("Третья октава");
+            ConsoleKeyInfo polz = Console.ReadKey();
+            if (polz.Key == ConsoleKey.Q)
+            {
+                Console.Beep(130, 1000);
+            }
+            
+        }
+    }
+}
---
+++ ежедневник/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ежедневник
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int position = 1;
+            Menu();
+            while (true)
+            {
+                ConsoleKeyInfo key = Console.ReadKey();
+                if (key .Key == ConsoleKey.UpArrow)
+                {
+                    position--;
+                }
+                else if (key.Key == ConsoleKey.DownArrow)
+                {
+                    position++;
+                }
+                Console.Clear();
+                Menu();
+                Console.SetCursorPosition(0, position);
+                Console.WriteLine("->");
+            }
+            static void Menu()
+            {
+                Console.WriteLine("  Выберите дату:");
+                Console.WriteLine("  01.02.2022");
+                 Console.WriteLine("  02.02.2022");
+                    Console.WriteLine("  03.03.2022");
+                        Console.WriteLine("  04.03.2022");
+                        Console.WriteLine("  05.03.2022");
+            }
+        }
+    }
+}
